73b840ef144f99fcfc2cc70be56fc7b5c5b22764
Diego Barneche <diego.barneche@ymail.com>
2016-07-14 13:10:13 +1000

    increase number of iterations in model fit

--- michaelisMentenFit.R
+++ michaelisMentenFit.R
@@ -12,8 +12,8 @@ mmJags       <-  list('boundedO2Vol'=o2tab$boundedO2Vol, 'id'=o2tab$sppNum, 'o2s
 mminits1     <-  list(lnA = 0.5,  lnB = 2, tauMu=1)
 mminits2     <-  list(lnA = 0,    lnB = 5, tauMu=3)
 mminits3     <-  list(lnA = -0.2, lnB = 3, tauMu=5)
-mmfit        <-  jags(data=mmJags, inits=list(mminits1, mminits2, mminits3), parameters.to.save=c('lnA', 'lnB', 'varMu', 'varR', 'r'), model.file='michaelisMenten.bug', n.chains=3, n.iter=5e5, DIC=TRUE, n.thin=500)
-mmfit        <-  autojags(mmfit, n.iter=5e5, n.thin=500, n.update=100)
+mmfit        <-  jags(data=mmJags, inits=list(mminits1, mminits2, mminits3), parameters.to.save=c('lnA', 'lnB', 'varMu', 'varR', 'r'), model.file='michaelisMenten.bug', n.chains=3, n.iter=1.5e6, DIC=TRUE, n.thin=375)
+mmfit        <-  autojags(mmfit, n.iter=1.5e6, n.thin=750, n.update=100)
 mmjagsout    <-  mmfit$BUGSoutput$summary #jags output
 
 rm(list=ls()[!(ls() %in% c('o2tab', 'mmfit', 'mmjagsout'))])
