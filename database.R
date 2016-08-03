8f19dd90b3e54950c4d1ce4527ac6d7556c3d62f
Diego Barneche <diego.barneche@ymail.com>
2016-08-03 13:56:37 +1000

    original data

--- database.R
+++ database.R
@@ -6,9 +6,7 @@ ast    <-  readFile('data/airSat_2015_12_17.csv')
 spid   <-  readFile('data/speciesID.csv')
 o2tab  <-  data.frame()
 for(i in 1:ncol(vol)) {
-	column   <-  as.numeric(sub('VO.', '', names(vol)[i]))
-	spMatch  <-  spid$column == column
-	dat      <-  data.frame(species=spid$species[spMatch], dry_mass_g=spid$dry_mass_g[spMatch], status=tolower(spid$status[spMatch]), shape=tolower(spid$shape[spMatch]), column=column, o2volume=vol[!is.na(vol[,i]),i], o2sat=ast[!is.na(ast[,i]),i], 	stringsAsFactors=FALSE)
+	dat  <-  data.frame(species=spid$species[spid$column==i], dry_mass_g=spid$dry_mass_g[spid$column==i], status=tolower(spid$status[spid$column==i]), shape=tolower(spid$shape[spid$column==i]), column=i, o2volume=vol[!is.na(vol[,i]),i], o2sat=ast[!is.na(ast[,i]),i], 	stringsAsFactors=FALSE)
 	dat$boundedO2Vol               <-  dat$o2volume / max(dat$o2volume)
  	o2tab    <-  rbind(o2tab, dat)
 }; rm(i, dat, vol, ast, spid)
